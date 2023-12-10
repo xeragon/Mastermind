c50a75c0efa021858cc591f0f906d3540496469c
= <=>
2023-12-10 11:12:15 +0100

    :memo: finition UML et redaction rapport1

---
+++ V1/Model/Color.java
@@ -0,0 +1,10 @@
+package V1.Model;
+
+public enum Color {
+    RED,
+    BLUE,
+    GREEN,
+    YELLOW,
+    PURPLE,
+    PINK
+}
---
+++ V1/Model/Combination.java
@@ -0,0 +1,7 @@
+package V1.Model;
+
+import java.util.ArrayList;
+
+public class Combination {
+    private ArrayList<Color> colors = new ArrayList<Color>();
+}
